98db1185cf10f9b8a068930b23ff7a66b39e9946
André Barcelos Silva <barcelos.ds@gmail.com>
2020-04-10 05:23:34 -0300

    Add docker_install.sh

---
+++ docker_install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# INSTALL DOCKER
+sudo apt remove docker docker-engine docker.io containerd runc
+sudo apt update
+sudo apt install -y apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
+sudo groupadd docker
+sudo usermod -aG docker $USER
+newgpr docker
+docker --version
+
+## Test
+# docker run --name hwtest hello-world
+# docker ps -a
+# docker rm hwtest
+
+# INSTALL DOCKER COMPOSER
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+docker-compose --version
